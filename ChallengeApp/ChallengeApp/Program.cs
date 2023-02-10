5c093c7c9b5a179c999e25012394246b226d86b4
Liliel7 <klaudia.lepka@gmail.com>
2023-02-10 16:30:13 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Klaudia";
+string sex = "kobieta";
+var age = 37;
+
+// Komunikat 1
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (sex == "kobieta" && age > 30)
+{
+    Console.WriteLine("Kobieta powyżej 30 lat");
+}
+else if (sex == "kobieta" && age == 30)
+{
+    Console.WriteLine("Kobieta 30 lat");
+}
+else if (sex == "mężczyzna" && age < 30)
+{
+    Console.WriteLine("Mężczyzna poniżej 30 lat");
+}
+else if (sex == "mężczyzna" && age > 30)
+{
+    Console.WriteLine("Mężczyzna powyżej 30 lat");
+}
+else if (sex == "mężczyzna" && age == 30)
+{
+    Console.WriteLine("Mężczyzna 30 lat");
+}
+
+// Komunikat 2
+Console.WriteLine(name + ", lat " + age);
+
+// Komunikat 3
+if (age < 18)
+{
+    if (sex == "kobieta")
+    {
+        Console.WriteLine("Niepełnoletnia kobieta");
+    }
+    else if (sex == "mężczyzna")
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
+else
+{
+    if (sex == "kobieta")
+    {
+        Console.WriteLine("Pełnoletnia kobieta");
+    }
+    else if (sex == "mężczyzna")
+    {
+        Console.WriteLine("Pełnoletni mężczyzna");
+    }
+}
